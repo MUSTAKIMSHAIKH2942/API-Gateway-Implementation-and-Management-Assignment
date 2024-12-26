8c80d56ce2fdd052f5fd817b8c3fa5b5e3a8ccec
Mustakim Shaikh <107025272+MUSTAKIMSHAIKH2942@users.noreply.github.com>
2024-12-26 14:01:08 +0530

    Update rbac.js

--- middleware/rbac.js
+++ middleware/rbac.js
@@ -1,12 +1,4 @@
 // middleware/rbac.js
 // This functionality is already in auth.js (authorizeRole).
 
-// -----------------------------------------------------------
-// middleware/transform.js
-// const transformRequestResponse = (req, res, next) => {
-//     req.headers['x-forwarded-for'] = req.ip;
-//     res.setHeader('x-powered-by', 'API Gateway');
-//     next();
-// };
 
-// module.exports = { transformRequestResponse };
